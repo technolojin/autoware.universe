a2ff7c6dbd5e8685805ea6a06c48cec8692d94d6
Taekjin LEE <taekjin.lee@tier4.jp>
2024-01-26 17:27:41 +0900

    feat: subscribe rois and camera info

--- perception/vision_roi_object_detector/src/vision_roi_object_detector/node.cpp
+++ perception/vision_roi_object_detector/src/vision_roi_object_detector/node.cpp
@@ -26,9 +26,9 @@ RoiObjectDetectorNode::RoiObjectDetectorNode(const rclcpp::NodeOptions & node_op
 : rclcpp::Node("vision_roi_object_detector_node", node_options),
   tf_buffer_(get_clock()),
   tf_listener_(tf_buffer_),
-  objects0_sub_(this, "input/cluster0", rclcpp::QoS{1}.get_rmw_qos_profile()),
-  objects1_sub_(this, "input/cluster1", rclcpp::QoS{1}.get_rmw_qos_profile()),
-  sync_(SyncPolicy(10), objects0_sub_, objects1_sub_)
+  rois_sub_(this, "input/rois", rclcpp::QoS{1}.reliable().get_rmw_qos_profile()),
+  camera_info_sub_(this, "input/camera_info", rclcpp::QoS{1}.best_effort().get_rmw_qos_profile()),
+  sync_(SyncPolicy(10), rois_sub_, camera_info_sub_)
 {
   output_frame_id_ = declare_parameter<std::string>("output_frame_id");
   using std::placeholders::_1;
@@ -36,35 +36,38 @@ RoiObjectDetectorNode::RoiObjectDetectorNode(const rclcpp::NodeOptions & node_op
   sync_.registerCallback(std::bind(&RoiObjectDetectorNode::objectsCallback, this, _1, _2));
 
   // Publisher
-  pub_objects_ = create_publisher<DetectedObjectsWithFeature>("~/output/clusters", rclcpp::QoS{1});
+  pub_objects_ = create_publisher<DetectedObjectsWithFeature>("~/output/roi_object", rclcpp::QoS{1});
 }
 
 void RoiObjectDetectorNode::objectsCallback(
-  const DetectedObjectsWithFeature::ConstSharedPtr & input_objects0_msg,
-  const DetectedObjectsWithFeature::ConstSharedPtr & input_objects1_msg)
+  const DetectedObjectsWithFeature::ConstSharedPtr & input_rois_msg,
+  const sensor_msgs::msg::CameraInfo::ConstSharedPtr & camera_info_msg)
 {
   if (pub_objects_->get_subscription_count() < 1) {
     return;
   }
-  // TODO(badai-nguyen): transform input topics to desired frame before concatenating
-  /* transform to the same with cluster0 frame id*/
-  DetectedObjectsWithFeature transformed_objects0;
-  DetectedObjectsWithFeature transformed_objects1;
-  if (
-    !object_recognition_utils::transformObjectsWithFeature(
-      *input_objects0_msg, output_frame_id_, tf_buffer_, transformed_objects0) ||
-    !object_recognition_utils::transformObjectsWithFeature(
-      *input_objects1_msg, output_frame_id_, tf_buffer_, transformed_objects1)) {
-    return;
-  }
+
+  // DetectedObjectsWithFeature transformed_objects;
+  // if (
+  //   !object_recognition_utils::transformObjectsWithFeature(
+  //     *input_rois_msg, output_frame_id_, tf_buffer_, transformed_objects)) {
+  //   return;
+  // }
+
+  // camera info dummy 
+  double num1 = camera_info_msg->k[0];
+  double num2 = camera_info_msg->k[4];
+  num1 = num1 + num2;
+
+  // debug message
+  RCLCPP_INFO(get_logger(), "num1: %f", num1);
 
   DetectedObjectsWithFeature output_objects;
-  output_objects.header = input_objects0_msg->header;
-  // add check frame id and transform if they are different
-  output_objects.feature_objects = transformed_objects0.feature_objects;
-  output_objects.feature_objects.insert(
-    output_objects.feature_objects.end(), transformed_objects1.feature_objects.begin(),
-    transformed_objects1.feature_objects.end());
+  output_objects.header = input_rois_msg->header;
+
+
+  // output_objects.feature_objects = transformed_objects.feature_objects;
+
   pub_objects_->publish(output_objects);
 }
 }  // namespace vision_roi_object_detector
