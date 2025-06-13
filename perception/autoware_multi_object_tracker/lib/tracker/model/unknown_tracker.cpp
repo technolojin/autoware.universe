d7e5c4663c2c86f24f4070a1045c97beb521d000
Taekjin LEE <taekjin.lee@tier4.jp>
2025-06-13 12:59:46 +0900

    fix(unknown_tracker): update motion model parameters for enhanced tracking accuracy
    - Increased the standard deviation values for the motion model parameters in the UnknownTracker to improve tracking performance.
    - Adjusted q_stddev_x and q_stddev_y to 1.5, and q_stddev_vx and q_stddev_vy to 9.8 * 0.5, optimizing the model for better responsiveness.
    
    These changes refine the motion model's effectiveness, particularly in dynamic tracking scenarios.

--- perception/autoware_multi_object_tracker/lib/tracker/model/unknown_tracker.cpp
+++ perception/autoware_multi_object_tracker/lib/tracker/model/unknown_tracker.cpp
@@ -47,10 +47,10 @@ UnknownTracker::UnknownTracker(
   if (enable_velocity_estimation_) {
     // Set motion model parameters
     {
-      constexpr double q_stddev_x = 0.5;         // [m/s]
-      constexpr double q_stddev_y = 0.5;         // [m/s]
-      constexpr double q_stddev_vx = 9.8 * 0.3;  // [m/(s*s)]
-      constexpr double q_stddev_vy = 9.8 * 0.3;  // [m/(s*s)]
+      constexpr double q_stddev_x = 1.5;         // [m/s]
+      constexpr double q_stddev_y = 1.5;         // [m/s]
+      constexpr double q_stddev_vx = 9.8 * 0.5;  // [m/(s*s)]
+      constexpr double q_stddev_vy = 9.8 * 0.5;  // [m/(s*s)]
       motion_model_.setMotionParams(q_stddev_x, q_stddev_y, q_stddev_vx, q_stddev_vy);
     }
 
