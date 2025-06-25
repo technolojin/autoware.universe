62dedc8c582786b9597da572286f35b600e79c35
ktro2828 <kotaro.uetake@tier4.jp>
2025-06-26 02:56:27 +0900

    test: update unit test
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- perception/autoware_simpl/test/test_preprocessor.cpp
+++ perception/autoware_simpl/test/test_preprocessor.cpp
@@ -17,6 +17,7 @@
 #include <gtest/gtest.h>
 
 #include <cmath>
+#include <string>
 #include <vector>
 
 namespace autoware::simpl::test
@@ -91,5 +92,147 @@ TEST(TestPreProcessor, ProcessMinimalInput)
   // Distance to another node should be positive
   size_t idx = (0 * (N + K) + 1) * 5 + 4;
   EXPECT_GT(rpe[idx], 0.0f);
+}
+
+class PreProcessorTest : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    setup_agent();
+    setup_map();
+  }
+
+  static constexpr size_t num_agent = 1;
+  static constexpr size_t num_past = 3;
+  static constexpr size_t num_polyline = 1;
+  static constexpr size_t num_point = 10;
+  static constexpr double polyline_range_distance = 100.0;
+  static constexpr double polyline_break_distance = 1.0;
+
+  std::vector<size_t> label_ids;
+  std::vector<AgentHistory> histories;
+  std::vector<Polyline> polylines;
+  AgentState current_ego;
+
+private:
+  void setup_agent()
+  {
+    label_ids = archetype::to_label_ids({"VEHICLE", "PEDESTRIAN", "CYCLIST"});
+    std::vector<std::pair<std::string, archetype::AgentLabel>> agent_id_and_label{
+      {"a", archetype::AgentLabel::VEHICLE},
+      {"b", archetype::AgentLabel::PEDESTRIAN},
+    };
+
+    for (const auto & [agent_id, label] : agent_id_and_label) {
+      archetype::AgentHistory history(agent_id, num_past);
+      for (size_t t = 0; t < num_past; ++t) {
+        history.update(archetype::AgentState(1, 1, 1, 1, 1, 1, label, true));
+      }
+      histories.emplace_back(history);
+    }
+
+    current_ego = archetype::AgentState(1, 1, 1, 1, 1, 1, AgentLabel::VEHICLE, true);
+  }
+
+  void setup_map()
+  {
+    // lane
+    archetype::Polyline lane{{
+      {1.0, 1.0, 1.0, archetype::MapLabel::ROADWAY},
+      {1.0, 1.0, 1.0, archetype::MapLabel::ROADWAY},
+      {1.0, 1.0, 1.0, archetype::MapLabel::ROADWAY},
+    }};
+    polylines.emplace_back(lane);
+
+    // left boundary
+
+    archetype::Polyline left_boundary{{
+      {1.0, 1.0, 1.0, archetype::MapLabel::SOLID},
+      {1.0, 1.0, 1.0, archetype::MapLabel::SOLID},
+    }};
+    polylines.emplace_back(left_boundary);
+
+    // right boundary
+    archetype::Polyline right_boundary{{{1.0, 1.0, 1.0, archetype::MapLabel::DASHED}}};
+    polylines.emplace_back(right_boundary);
+
+    // crosswalk
+    archetype::Polyline crosswalk{{
+      {1.0, 1.0, 1.0, archetype::MapLabel::CROSSWALK},
+      {1.0, 1.0, 1.0, archetype::MapLabel::CROSSWALK},
+      {1.0, 1.0, 1.0, archetype::MapLabel::CROSSWALK},
+      {1.0, 1.0, 1.0, archetype::MapLabel::CROSSWALK},
+      {1.0, 1.0, 1.0, archetype::MapLabel::CROSSWALK},
+    }};
+    polylines.emplace_back(crosswalk);
+  }
+};
+
+TEST_F(PreProcessorTest, PreProcess)
+{
+  PreProcessor preprocessor(
+    label_ids, num_agent, num_past, num_polyline, num_point, polyline_range_distance,
+    polyline_break_distance);
+
+  const auto [agent_metadata, map_metadata, rpe_tensor] =
+    preprocessor.process(histories, polylines, current_ego);
+
+  // clang-format off
+  std::vector<float> expected_agent = {
+    0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f,
+    1.0f, 1.0f, 1.0f,
+    0.0f, 0.0f, 0.0f,
+    1.3817732f, 1.3817732f, 1.3817732f,
+    -0.30116862f, -0.30116862f, -0.30116862f,
+    1.0f, 1.0f, 1.0f,
+    0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f,
+    1.0f, 1.0f, 1.0f,
+  };
+
+  std::vector<float> expected_map = {
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f
+  };
+
+  std::vector<float> expected_rpe = {
+    1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f
+  };
+  // clang-format on
+
+  ASSERT_EQ(agent_metadata.tensor.size(), expected_agent.size());
+  ASSERT_EQ(map_metadata.tensor.size(), expected_map.size());
+  ASSERT_EQ(rpe_tensor.size(), expected_rpe.size());
+
+  for (size_t i = 0; i < agent_metadata.tensor.size(); ++i) {
+    auto answer = *(agent_metadata.tensor.data() + i);
+    auto & expect = expected_agent[i];
+    EXPECT_NEAR(answer, expect, 1e-5);
+  }
+
+  for (size_t i = 0; i < map_metadata.tensor.size(); ++i) {
+    auto answer = *(map_metadata.tensor.data() + i);
+    auto & expect = expected_map[i];
+    EXPECT_NEAR(answer, expect, 1e-5);
+  }
+
+  for (size_t i = 0; i < rpe_tensor.size(); ++i) {
+    auto & answer = rpe_tensor[i];
+    auto & expect = expected_rpe[i];
+    EXPECT_NEAR(answer, expect, 1e-5);
+  }
 }
 }  // namespace autoware::simpl::test
